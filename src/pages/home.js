15729c6075226ae54dba1a376f2c223bdd4c10a6
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-09-22 13:45:18 +0200

    Add elements in the homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,13 +4,27 @@
  */
 function getHomepage() {
     const home = document.createElement("div");
-    home.classList.add("home");
-    home.innerText = "This is homepage";
+    home.classList.add("home-content");
+
+    const top_paragraph = document.createElement("p");
+    top_paragraph.innerText = "Get the best food in the sea.";
+    
+    const bottom_paragaph = document.createElement("p");
+    bottom_paragaph.innerText = "Visit us in our ship: Thousand Sunny";
+
+    const imageHome = document.createElement("img");
+    imageHome.src = "assets/images/home_image.jpeg";
+    imageHome.alt = "Sanji holding a dish.";
+
+    home.appendChild(top_paragraph);
+    home.appendChild(imageHome);
+    home.appendChild(bottom_paragaph);
+
     return home;
 }
 
 function loadHomepage() {
-    const content = document.getElementById("#content");
+    const content = document.getElementById("content");
     content.innerHTML = ""; // Clear content
     content.appendChild(getHomepage())
 }

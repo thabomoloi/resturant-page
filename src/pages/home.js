f7c96ed7f65e2533574b40bea1f39a9fd5ad9b55
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-09-26 21:35:42 +0200

    Add media query event listener for image

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,19 +6,37 @@ function getHomepage() {
     const home = document.createElement("div");
     home.classList.add("home-content");
 
-    const top_paragraph = document.createElement("p");
-    top_paragraph.innerText = "Get the best food in the sea.";
+    const h1 = document.createElement("h1");
+    h1.innerText = "Sunny Eats Resturants";
+    h1.style.color = "white";
+    h1.style.textAlign = "center"
+
+    const top_paragraph1 = document.createElement("p");
+    top_paragraph1.innerText = "Get the best food in the sea.";
     
-    const bottom_paragaph = document.createElement("p");
-    bottom_paragaph.innerText = "Visit us in our ship: The Thousand Sunny";
+    const bottom_paragaph1 = document.createElement("p");
+    bottom_paragaph1.innerText = "Visit us in our ship: The Thousand Sunny";
 
     const imageHome = document.createElement("img");
-    imageHome.src = "./images/home_image.jpeg";
+    
+    const image = (x) => {
+        if (x.matches) 
+            imageHome.src = "./images/home_image.jpeg";
+        else
+            imageHome.src = "./images/home_image_mobile.jpeg";
+    }
     imageHome.alt = "Sanji holding a dish.";
 
-    home.appendChild(top_paragraph);
+    var query = window.matchMedia("(min-width: 600px)");
+    image(query);
+    query.addEventListener('change', (event) => {
+        image(event.target);
+    })
+    
+    home.appendChild(h1);
+    home.appendChild(top_paragraph1);
     home.appendChild(imageHome);
-    home.appendChild(bottom_paragaph);
+    home.appendChild(bottom_paragaph1);
 
     return home;
 }

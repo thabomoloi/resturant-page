83098692df6bdccbab58a8c4306df99dc0e2f35f
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-09-26 22:08:52 +0200

    Call load footer function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import loadHeader from "./header_footer/header";
 import loadHomepage from "./pages/home";
-
+import loadFooter from "./header_footer/footer";
 
 loadHeader();
 loadHomepage();
+loadFooter();
+

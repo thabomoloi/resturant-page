a80fba86be2f5f85f670fd05db87d2dfee2d8478
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2022-09-27 12:42:13 +0200

    Create menu items

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,5 +1,36 @@
 function getMenuPage() {
+    const container = document.createElement("div");
+    container.className = "menu-content";
 
+    const h1 = document.createElement("h1");
+    h1.innerText = "Menu";
+    container.appendChild(h1);
+
+    const menu = document.createElement("div");
+    menu.className = "menu";
+
+    const menuItems = [
+        {"name": "Curry Rice", "src": "../images/cuury_rice.jpg", "alt": "Curry Rice"},
+        {"name": "Ramen", "src": "../images/ramen.jpg", "alt": "Ramen"},
+        {"name": "Seafood Fried Rice", "src": "../images/seafood_fried_rice.jpeg", "alt": "Seafood Fried Rice"}
+     ]
+
+    for (let i = 0; i < 3; i++) {
+        const item = menuItems[i];
+        const menuDiv = document.createElement("div");
+        menuDiv.className = "menu-item";
+
+        const h3 = document.createElement("h3");
+        h3.innerText = item.name;
+        menuDiv.appendChild(h2);
+
+        const img = document.createElement("img");
+        img.src = item.src;
+        img.alt = item.alt;
+        menuDiv.appendChild(img);
+    }
+    container.appendChild(menuItems);
+    return container;
 }
 
 function loadMenu() {
